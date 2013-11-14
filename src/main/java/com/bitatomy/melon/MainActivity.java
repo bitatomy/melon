112770b30e27f4313c13675320d5fb59fcc3252d
Jatinder Singh <bitatomy@gmail.com>
2013-11-13 23:43:08 -0500

    Creates main activity and package structure

---
+++ src/main/java/com/bitatomy/melon/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bitatomy.melon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author jsingh on 11/13/2013 at 11:41 PM
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
